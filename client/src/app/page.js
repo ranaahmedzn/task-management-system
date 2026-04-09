413205d28b92682f1dddf310cb0340825796f954
Rana Ahmed <ranasheikhsbd@gmail.com>
2026-04-09 10:29:12 +0600

    Initial project setup with client and empty server structure

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Welcome to the Task Management System.
+      </h1>
+    </div>
+  );
+}
